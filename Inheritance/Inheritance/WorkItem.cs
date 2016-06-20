e52386a0ae5ba714c85bd26b8a462377a0fdf78d
Ronda Bergman <rondabergman@hotmail.com>
2016-06-20 11:30:37 -0400

    Adding inheritance

---
+++ Inheritance/Inheritance/WorkItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class WorkItem : object
+    {
+        // Static field currentID stores the job ID of the last WorkItem that had been created
+        private static int currentID;
+
+        //Properties
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TimeSpan JobLength { get; set; }
+
+        
+    }
+}
