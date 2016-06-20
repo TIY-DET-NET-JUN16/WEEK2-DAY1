095f9434bc904e0f5d8c434d51a34e7ff424b20c
Ronda Bergman <rondabergman@hotmail.com>
2016-06-20 13:36:33 -0400

    Daily Commit

---
+++ StaticClasses/StaticClasses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Utilities;
+
+namespace StaticClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = Utils.Addition(2, 5);
+
+            try
+            {
+                NullReferenceException test = new NullReferenceException("This is the message in my exception");
+                throw test;
+            }
+            catch(Exception ex)
+            {
+                string temp = ex.Message;
+            }
+        }
+    }
+}
